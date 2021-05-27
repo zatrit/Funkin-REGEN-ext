a468c5a49c2df274c285ac9aca55e7391265d33b
zatrit <52380960+zatrit@users.noreply.github.com>
2021-05-27 21:45:36 +0300

    Fixed PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -19,7 +19,7 @@ class PauseSubState extends MusicBeatSubstate
 {
 	var grpMenuShit:FlxTypedGroup<Alphabet>;
 
-	var menuItems:Array<String> = ['Resume', 'Restart Song', #if mobile "Charting editor", #end 'Exit to menu'];
+	var menuItems:Array<String> = ['Resume', 'Restart Song', #if !mobile "Charting editor", #end 'Exit to menu'];
 	var curSelected:Int = 0;
 
 	var pauseMusic:FlxSound;
