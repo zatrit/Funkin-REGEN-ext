be4cdff758930a7eb4ecec21d5fbec179102b699
zatrit <52380960+zatrit@users.noreply.github.com>
2021-05-30 20:58:48 +0300

    Now it's fully working mod with Garcello, what about Whitty?

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -88,7 +88,6 @@ class FreeplayState extends MusicBeatState
 			addWeek(['Senpai', 'Roses', 'Thorns'], 6, ['senpai', 'senpai', 'spirit']);
 		if (StoryMenuState.weekUnlocked[7] || isDebug)
 			addWeek(['Headache', 'Nerves', 'Release'#if (debug&&desktop) , 'Fading' #end], 7, ['garcello', 'garcellotired', 'garcellodead' #if (debug&&desktop) , 'garcelloghosty' #end]);
-		}
 
 		// LOAD MUSIC
 
