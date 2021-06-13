2e93570a04975120d1003b8d782afd9055ad403b
zatrit <52380960+zatrit@users.noreply.github.com>
2021-06-13 19:31:51 +0300

    Kapi is finished, finally

--- source/OutdatedSubState.hx
+++ source/OutdatedSubState.hx
@@ -12,38 +12,58 @@ class OutdatedSubState extends MusicBeatState
 
 	override function create()
 	{
-		#if mobile
-		leftState = true;
-		FlxG.switchState(new MainMenuState());
-		#else
 		super.create();
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
-		var ver = "v" + Application.current.meta.get('version');
+
+		var http = new haxe.Http("https://raw.githubusercontent.com/zatrit/Funkin-REGEN-ext/master/version.downloadMe");
+		var githubVersion:String;
+		var ver = Application.current.meta.get('version');
+			
+		http.onData = function (data:String)
+		{
+			githubVersion=data;
+		}
+		http.onError = (msg:String)->{
+			githubVersion="";
+			trace(msg);
+		};
+		http.request();
+
 		var txt:FlxText = new FlxText(0, 0, FlxG.width,
 			"HEY! You're running an outdated version of the game!\nCurrent version is "
 			+ ver
 			+ " while the most recent version is "
-			+ NGio.GAME_VER
-			+ "! Press SPACE to go to itch.io, or ESCAPE to ignore this!!",
-			32);
+			+ githubVersion
+			#if !mobile
+			+ "! Press SPACE to go to github repo, or ESCAPE to ignore this!!"
+			#else
+			+ "! Touch screen to ignore this!!"
+			#end
+			,32);
 		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		txt.screenCenter();
 		add(txt);
-		#end
 	}
 
 	override function update(elapsed:Float)
 	{
-		if (controls.ACCEPT)
+		if (controls.ACCEPT#if mobile || FlxG.touches.justStarted().length>0 #end)
 		{
-			FlxG.openURL("https://ninja-muffin24.itch.io/funkin");
+			#if !mobile
+			FlxG.openURL("https://github.com/zatrit/Funkin-REGEN-ext");
+			#else
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+			#end
 		}
+		#if !mobile
 		if (controls.BACK)
 		{
 			leftState = true;
 			FlxG.switchState(new MainMenuState());
 		}
+		#end
 		super.update(elapsed);
 	}
 }
